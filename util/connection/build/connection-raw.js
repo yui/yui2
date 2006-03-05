2d4e35e3e1589d114a3971aa930a9e6bd9786da2
Thomas Sha <tsha@yahoo-inc.com>
2006-03-05 22:31:48 +0000

    Added attribution

--- util/connection/build/connection-raw.js
+++ util/connection/build/connection-raw.js
@@ -1,6 +1,8 @@
 /*
-Copyright (c) 2006 Yahoo! Inc. All rights reserved.
-version 0.10.0
+Copyright (c) 2006, Yahoo! Inc. All rights reserved.
+Code licensed under the BSD License:
+http://developer.yahoo.net/yui/license.txt
+Version: 0.10.0
 */
 
 /**
