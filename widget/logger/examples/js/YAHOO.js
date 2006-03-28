2fc6391815b8c31633374b9087bcd7e533687d63
Jenny Han-Donnelly <jennyhan@yahoo-inc.com>
2006-03-28 19:08:53 +0000

    Initial checkin of logger widget.

---
+++ widget/logger/examples/js/YAHOO.js
@@ -0,0 +1,65 @@
+/* Copyright (c) 2006 Yahoo! Inc. All rights reserved. */
+
+/**
+ * The Yahoo global namespace
+ * @constructor
+ */
+var YAHOO = window.YAHOO || {};
+
+/**
+ * Returns the namespace specified and creates it if it doesn't exist
+ *
+ * YAHOO.namespace("property.package");
+ * YAHOO.namespace("YAHOO.property.package");
+ *
+ * Either of the above would create YAHOO.property, then
+ * YAHOO.property.package
+ *
+ * @param  {String} sNameSpace String representation of the desired 
+ *                             namespace
+ * @return {Object}            A reference to the namespace object
+ */
+YAHOO.namespace = function( sNameSpace ) {
+
+    if (!sNameSpace || !sNameSpace.length) {
+        return null;
+    }
+
+    var levels = sNameSpace.split(".");
+
+    var currentNS = YAHOO;
+
+    // YAHOO is implied, so it is ignored if it is included
+    for (var i=(levels[0] == "YAHOO") ? 1 : 0; i<levels.length; ++i) {
+        currentNS[levels[i]] = currentNS[levels[i]] || {};
+        currentNS = currentNS[levels[i]];
+    }
+
+    return currentNS;
+};
+
+YAHOO.namespace("util");
+YAHOO.namespace("widget");
+YAHOO.namespace("example");
+
+/**
+ * Global logger object
+ */
+YAHOO.logger = function(sName) {
+    if(YAHOO.widget.Logger) {
+        return new YAHOO.widget.Logwriter(sName);
+    }
+    else {
+        return false;
+    }
+};
+
+/**
+ * Global log method. Validates for namespace.
+ */
+YAHOO.log = function(sMsg,sType) {
+    if(YAHOO.widget.Logger) {
+        YAHOO.widget.Logger.log(null, sMsg,sType);
+    }
+    else return false;
+};
