8812bcb1c38d5cc220217c413f144c293683afcd
peterson <peterson>
2006-05-04 18:27:59 +0000

    *** empty log message ***

---
+++ examples/container/php/post.php
@@ -0,0 +1,32 @@
+<?php
+print "data = {";
+
+$index = 0;
+
+foreach($_POST as $key => $value) {
+	
+	print "$key:";
+    
+	if (gettype($value) == "array") {
+		print "[";
+		for ($i = 0;$i < count($_POST["$key"]);$i++) { 
+		   $v = $_POST["$key"][$i]; 
+		   print "\"$v\"";
+		   if ($i < count($_POST["$key"])-1) {
+				print ",";
+		   }
+		} 
+		print "]";
+	} else {
+		print "\"$value\"";
+	}
+
+	$index++;
+
+	if ( ($index) < count($_POST) ) {
+		print ",";
+	}
+}
+
+print "}";
+?> 
