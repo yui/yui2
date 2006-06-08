6b6aef93fc57e0852a49cc9170e6ee5a8df14f8e
Adam Moore <adamoore@yahoo-inc.com>
2006-06-08 21:27:47 +0000

    Logger update

--- build/yahoo/yahoo-debug.js
+++ build/yahoo/yahoo-debug.js
@@ -1,3 +1,4 @@
+
 /*                                                                                                                                                      
 Copyright (c) 2006, Yahoo! Inc. All rights reserved.                                                                                                    
 Code licensed under the BSD License:                                                                                                                    
