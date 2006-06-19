851136f78bdb6642d5b08039d5e0b713c73ffd1d
Matt Sweeney <msweeney@yahoo-inc.com>
2006-06-19 23:54:57 +0000

    replaced with Penner's

--- util/animation/src/js/Easing.js
+++ util/animation/src/js/Easing.js
@@ -1,14 +1,19 @@
 /*
-Copyright (c) 2006, Yahoo! Inc. All rights reserved.
-Code licensed under the BSD License:
-http://developer.yahoo.net/yui/license.txt
-Version: 0.10.0
+TERMS OF USE - EASING EQUATIONS
+Open source under the BSD License.
+Copyright © 2001 Robert Penner All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 */
 
-/**
- * @class Class for defining the acceleration rate and path of animations.
- */
-YAHOO.util.Easing = new function() {
+YAHOO.util.Easing = {
 
    /**
     * Uniform speed between points.
@@ -18,89 +23,223 @@ YAHOO.util.Easing = new function() {
     * @param {Number} d Total length of animation.
     * @return {Number} The computed value for the current animation frame.
     */
-   this.easeNone = function(t, b, c, d) {
-	return b+c*(t/=d); 
-   };
+   easeNone: function (t, b, c, d) {
+   	return c*t/d + b;
+   },
    
    /**
-    * Begins slowly and accelerates towards end.
+    * Begins slowly and accelerates towards end. (quadratic)
     * @param {Number} t Time value used to compute current value.
     * @param {Number} b Starting value.
     * @param {Number} c Delta between start and end values.
     * @param {Number} d Total length of animation.
     * @return {Number} The computed value for the current animation frame.
     */
-   this.easeIn = function(t, b, c, d) {
-   	return b+c*((t/=d)*t*t);
-   };
-   
+   easeIn: function (t, b, c, d) {
+   	return c*(t/=d)*t + b;
+   },
+
    /**
-    * Begins quickly and decelerates towards end.
+    * Begins quickly and decelerates towards end.  (quadratic)
     * @param {Number} t Time value used to compute current value.
     * @param {Number} b Starting value.
     * @param {Number} c Delta between start and end values.
     * @param {Number} d Total length of animation.
     * @return {Number} The computed value for the current animation frame.
     */
-   this.easeOut = function(t, b, c, d) {
-   	var ts=(t/=d)*t;
-   	var tc=ts*t;
-   	return b+c*(tc + -3*ts + 3*t);
-   };
+   easeOut: function (t, b, c, d) {
+   	return -c *(t/=d)*(t-2) + b;
+   },
    
    /**
-    * Begins slowly and decelerates towards end.
+    * Begins slowly and decelerates towards end. (quadratic)
     * @param {Number} t Time value used to compute current value.
     * @param {Number} b Starting value.
     * @param {Number} c Delta between start and end values.
     * @param {Number} d Total length of animation.
     * @return {Number} The computed value for the current animation frame.
     */
-   this.easeBoth = function(t, b, c, d) {
-   	var ts=(t/=d)*t;
-   	var tc=ts*t;
-   	return b+c*(-2*tc + 3*ts);
-   };
+   easeBoth: function (t, b, c, d) {
+   	if ((t/=d/2) < 1) return c/2*t*t + b;
+   	return -c/2 * ((--t)*(t-2) - 1) + b;
+   },
    
    /**
-    * Begins by going below staring value.
+    * Begins slowly and accelerates towards end. (quartic)
     * @param {Number} t Time value used to compute current value.
     * @param {Number} b Starting value.
     * @param {Number} c Delta between start and end values.
     * @param {Number} d Total length of animation.
     * @return {Number} The computed value for the current animation frame.
     */
-   this.backIn = function(t, b, c, d) {
-   	var ts=(t/=d)*t;
-   	var tc=ts*t;
-   	return b+c*(-3.4005*tc*ts + 10.2*ts*ts + -6.2*tc + 0.4*ts);
-   };
+   easeInStrong: function (t, b, c, d) {
+   	return c*(t/=d)*t*t*t + b;
+   },
    
    /**
-    * End by going beyond ending value.
+    * Begins quickly and decelerates towards end.  (quartic)
     * @param {Number} t Time value used to compute current value.
     * @param {Number} b Starting value.
     * @param {Number} c Delta between start and end values.
     * @param {Number} d Total length of animation.
     * @return {Number} The computed value for the current animation frame.
     */
-   this.backOut = function(t, b, c, d) {
-   	var ts=(t/=d)*t;
-   	var tc=ts*t;
-   	return b+c*(8.292*tc*ts + -21.88*ts*ts + 22.08*tc + -12.69*ts + 5.1975*t);
-   };
+   easeOutStrong: function (t, b, c, d) {
+   	return -c * ((t=t/d-1)*t*t*t - 1) + b;
+   },
    
    /**
-    * Starts by going below staring value, and ends by going beyond ending value.
+    * Begins slowly and decelerates towards end. (quartic)
     * @param {Number} t Time value used to compute current value.
     * @param {Number} b Starting value.
     * @param {Number} c Delta between start and end values.
     * @param {Number} d Total length of animation.
     * @return {Number} The computed value for the current animation frame.
     */
-   this.backBoth = function(t, b, c, d) {
-   	var ts=(t/=d)*t;
-   	var tc=ts*t;
-   	return b+c*(0.402*tc*ts + -2.1525*ts*ts + -3.2*tc + 8*ts + -2.05*t);
-   };
+   easeBothStrong: function (t, b, c, d) {
+   	if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+   	return -c/2 * ((t-=2)*t*t*t - 2) + b;
+   },
+
+   /**
+    * snap in elastic effect
+    * @param {Number} t Time value used to compute current value.
+    * @param {Number} b Starting value.
+    * @param {Number} c Delta between start and end values.
+    * @param {Number} d Total length of animation.
+    * @param {Number} p Period (optional)
+    * @return {Number} The computed value for the current animation frame.
+    */
+
+   elasticIn: function (t, b, c, d, a, p) {
+   	if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+   	if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
+   	else var s = p/(2*Math.PI) * Math.asin (c/a);
+   	return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+   },
+
+   /**
+    * snap out elastic effect
+    * @param {Number} t Time value used to compute current value.
+    * @param {Number} b Starting value.
+    * @param {Number} c Delta between start and end values.
+    * @param {Number} d Total length of animation.
+    * @param {Number} p Period (optional)
+    * @return {Number} The computed value for the current animation frame.
+    */
+   elasticOut: function (t, b, c, d, a, p) {
+   	if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+   	if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
+   	else var s = p/(2*Math.PI) * Math.asin (c/a);
+   	return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+   },
+   
+   /**
+    * snap both elastic effect
+    * @param {Number} t Time value used to compute current value.
+    * @param {Number} b Starting value.
+    * @param {Number} c Delta between start and end values.
+    * @param {Number} d Total length of animation.
+    * @param {Number} p Period (optional)
+    * @return {Number} The computed value for the current animation frame.
+    */
+   elasticBoth: function (t, b, c, d, a, p) {
+   	if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+   	if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
+   	else var s = p/(2*Math.PI) * Math.asin (c/a);
+   	if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+   	return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+   },
+
+
+   /**
+    * back easing in - backtracking slightly, then reversing direction and moving to target
+    * @param {Number} t Time value used to compute current value.
+    * @param {Number} b Starting value.
+    * @param {Number} c Delta between start and end values.
+    * @param {Number} d Total length of animation.
+    * @param {Number) s Overshoot (optional)
+    * @return {Number} The computed value for the current animation frame.
+    */
+   backIn: function (t, b, c, d, s) {
+   	if (typeof s == undefined) s = 1.70158;
+   	return c*(t/=d)*t*((s+1)*t - s) + b;
+   },
+
+   /**
+    * back easing out - moving towards target, overshooting it slightly,
+    * then reversing and coming back to target
+    * @param {Number} t Time value used to compute current value.
+    * @param {Number} b Starting value.
+    * @param {Number} c Delta between start and end values.
+    * @param {Number} d Total length of animation.
+    * @param {Number) s Overshoot (optional)
+    * @return {Number} The computed value for the current animation frame.
+    */
+   backOut: function (t, b, c, d, s) {
+   	if (typeof s == undefined) s = 1.70158;
+   	return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+   },
+   
+   /**
+    * back easing in/out - backtracking slightly, then reversing direction and moving to target,
+    * then overshooting target, reversing, and finally coming back to target
+    * @param {Number} t Time value used to compute current value.
+    * @param {Number} b Starting value.
+    * @param {Number} c Delta between start and end values.
+    * @param {Number} d Total length of animation.
+    * @param {Number) s Overshoot (optional)
+    * @return {Number} The computed value for the current animation frame.
+    */
+   backBoth: function (t, b, c, d, s) {
+   	if (typeof s == undefined) s = 1.70158; 
+   	if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+   	return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+   },
+
+   /**
+    * bounce in
+    * @param {Number} t Time value used to compute current value.
+    * @param {Number} b Starting value.
+    * @param {Number} c Delta between start and end values.
+    * @param {Number} d Total length of animation.
+    * @return {Number} The computed value for the current animation frame.
+    */
+   bounceIn: function (t, b, c, d) {
+   	return c - YAHOO.util.Easing.bounceOut(d-t, 0, c, d) + b;
+   },
+   
+   /**
+    * bounce out
+    * @param {Number} t Time value used to compute current value.
+    * @param {Number} b Starting value.
+    * @param {Number} c Delta between start and end values.
+    * @param {Number} d Total length of animation.
+    * @return {Number} The computed value for the current animation frame.
+    */
+   bounceOut: function (t, b, c, d) {
+   	if ((t/=d) < (1/2.75)) {
+   		return c*(7.5625*t*t) + b;
+   	} else if (t < (2/2.75)) {
+   		return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+   	} else if (t < (2.5/2.75)) {
+   		return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+   	} else {
+   		return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+   	}
+   },
+   
+   /**
+    * bounce both
+    * @param {Number} t Time value used to compute current value.
+    * @param {Number} b Starting value.
+    * @param {Number} c Delta between start and end values.
+    * @param {Number} d Total length of animation.
+    * @return {Number} The computed value for the current animation frame.
+    */
+   bounceBoth: function (t, b, c, d) {
+   	if (t < d/2) return YAHOO.util.Easing.bounceIn(t*2, 0, c, d) * .5 + b;
+   	return YAHOO.util.Easing.bounceOut(t*2-d, 0, c, d) * .5 + c*.5 + b;
+   }
 };
+
