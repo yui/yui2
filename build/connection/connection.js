84c9f53d329f6b3edfd2a930722bbe9801f75e2c
Thomas Sha <tsha@yahoo-inc.com>
2006-07-10 15:40:54 +0000

    2.0 GA API revisions to abort and setForm

--- build/connection/connection.js
+++ build/connection/connection.js
@@ -39,7 +39,7 @@ YAHOO.util.Connect =
 	_has_http_headers:false,
 
  /**
-  * Property that that determines if a default header of
+  * Determines if a default header of
   * Content-Type of 'application/x-www-form-urlencoded'
   * will be added to any client HTTP headers sent for POST
   * transactions.
@@ -323,6 +323,7 @@ YAHOO.util.Connect =
    * @private
    * @param {object} o The connection object
    * @param {object} callback - User-defined callback object
+   * @param {boolean} determines if the transaction was aborted.
    * @return void
    */
     handleTransactionResponse:function(o, callback, isAbort)
@@ -630,6 +631,7 @@ YAHOO.util.Connect =
    * document upon completion of the upload transaction.
    *
    * @private
+   * @param {string} optional qualified path of iframe resource for SSL in IE.
    * @return void
    */
 	createFrame:function(secureUri){
@@ -714,6 +716,8 @@ YAHOO.util.Connect =
    * Public method to terminate a transaction, if it has not reached readyState 4.
    * @public
    * @param {object} o The connection object returned by asyncRequest.
+   * @param {object} callback  User-defined callback object.
+   * @param {string} isTimeout boolean to indicate if abort was a timeout.
    * @return void
    */
 	abort:function(o, callback, isTimeout)
