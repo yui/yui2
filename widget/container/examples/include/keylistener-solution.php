4c0fa6147c8a8d6a6dd88c2ef5a9c0d2ad9a9365
peterson <peterson>
2006-11-11 00:41:50 +0000

    *** empty log message ***

--- widget/container/examples/include/keylistener-solution.php
+++ widget/container/examples/include/keylistener-solution.php
@@ -1,4 +1,4 @@
-			<p>You can see the full example in action below:</p>
+			<p>You can see the full example in action below. Note that you may need to give focus to the IFRAME by clicking it before the example will work.</p>
 			
 			<div id="solution" style="">
 				<iframe src="solution" style="width:96%;height:325px"></iframe>
