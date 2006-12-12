9e13dae0fffb7ccdaf858295bae92878734e55c2
Todd Kloots <kloots@yahoo-inc.com>
2006-12-12 19:23:10 +0000

    Shell script for running JSLint over the Menu source files

---
+++ widget/menu/lint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+echo contextmenu.js warnings:
+
+/usr/local/bin/jslint src/js/contextmenu.js
+
+echo contextmenuitem.js warnings:
+
+/usr/local/bin/jslint src/js/contextmenuitem.js
+
+echo menu.js warnings:
+
+/usr/local/bin/jslint src/js/menu.js
+
+echo menubar.js warnings:
+
+/usr/local/bin/jslint src/js/menubar.js
+
+echo menubaritem.js warnings:
+
+/usr/local/bin/jslint src/js/menubaritem.js
+
+echo menuitem.js warnings:
+
+/usr/local/bin/jslint src/js/menuitem.js
+
+echo menumanager.js warnings:
+
+/usr/local/bin/jslint src/js/menumanager.js
+
+echo menumodule.js warnings:
+
+/usr/local/bin/jslint src/js/menumodule.js
+
+echo menumoduleitem.js warnings:
+
+/usr/local/bin/jslint src/js/menumoduleitem.js
