56122f9b437519e573a6be371ba405e23acdaa83
Todd Kloots <kloots@yahoo-inc.com>
2007-02-02 02:31:13 +0000

    Updates

--- widget/menu/build.sh
+++ widget/menu/build.sh
@@ -1,75 +1,8 @@
 #!/bin/sh
 
-# Build the JavaScript
+# Build the JavaScript, images and CSS
 
-cat src/js/menumanager.js src/js/menu.js src/js/menumodule.js src/js/menuitem.js src/js/menumoduleitem.js src/js/contextmenu.js src/js/contextmenuitem.js src/js/menubar.js src/js/menubaritem.js > build/menu-lib.js
-
-cat copyright.txt build/menu-lib.js > build/menu-debug.js
-
-
-# Remove logger statements
-
-/usr/local/bin/perl -i -p00we 's/^.*?this\.log.*?(;|,|\).*;|(\n.*?)*?\).*;).*;?.*?\n/""/emg' build/menu-lib.js
-/usr/local/bin/perl -i -p00we 's/^.*?m_oLogger.*?(;|,|\).*;|(\n.*?)*?\).*;).*;?.*?\n/""/emg' build/menu-lib.js
-
-cat copyright.txt build/menu-lib.js > build/menu.js
-
-~/jsmin/jsmin < build/menu-lib.js > build/menu-compressed.js
-
-cat copyright.txt build/menu-compressed.js > build/menu-min.js
-
-
-# Run JSLint over the three build files
-
-echo menu-debug.js JSLint warnings:
-
-/usr/local/bin/jslint build/menu-debug.js
-
-echo menu-min.js JSLint  warnings:
-
-/usr/local/bin/jslint build/menu-min.js
-
-echo menu.js JSLint warnings:
-
-/usr/local/bin/jslint build/menu.js
-
-
-# Add the copyright notice to the CSS file
-
-cat copyright.txt src/css/menu.css > build/assets/menu.css
-
-
-# Remove temporary files
-
-rm copyright.txt
-rm build/menu-lib.js
-rm build/menu-compressed.js
-
-
-# Copy the JavaScript into the official build directories
-
-cp build/*.js ../../build/menu/
-cp build/*.js ~/dev/yahoo/properties/webservices/site/yui/build/menu/
-
-
-# Copy the CSS into the official build directories
-
-cp build/assets/menu.css ../../build/menu/assets
-cp build/assets/menu.css ~/dev/yahoo/properties/webservices/site/yui/build/menu/assets
-
-
-# Copy the image assets into the build directory
-
-cp src/img/*.gif build/assets/
-cp build/assets/*.gif ../../build/menu/assets
-cp build/assets/*.gif ~/dev/yahoo/properties/webservices/site/yui/build/menu/assets
-
-
-# Copy the README file to the build directory
-
-cp build/README ../../build/menu/
-cp build/README ~/dev/yahoo/properties/webservices/site/yui/build/menu/
-
+ant deploy
 
 # Create example files
 
@@ -100,18 +33,11 @@ php examples/topnavfromjswithanim.html > ../../examples/menu/topnavfromjswithani
 php examples/topnavfrommarkupwithanim.html > ../../examples/menu/topnavfrommarkupwithanim.html
 
 
-# Copy the examples into the build directory for YDN
-
-cp ../../examples/menu/*.html ~/dev/yahoo/properties/webservices/site/yui/examples/menu
-
-
 # Copy the image assets for the example files into the build directories
 
 cp examples/img/*.jpg ../../examples/menu/img
-cp examples/img/*.jpg ~/dev/yahoo/properties/webservices/site/yui/examples/menu/img
-
 cp examples/img/*.png ../../examples/menu/img
-cp examples/img/*.png ~/dev/yahoo/properties/webservices/site/yui/examples/menu/img
+
 
 # Generate the documentation
 
