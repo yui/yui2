f4a4d3e03d7dbab7f1b365050e2006241fcb1324
Todd Kloots <kloots@yahoo-inc.com>
2007-03-21 18:21:47 +0000

    Removed legacy build file

--- widget/container/src/js-debug/build-debug.bat
+++
@@ -1,3 +0,0 @@
-copy Config-debug.js + Module-debug.js + Overlay-debug.js + OverlayManager-debug.js + KeyListener-debug.js + ContainerEffect-debug.js /A container_core-debug.js
-
-copy Config-debug.js + Module-debug.js + Overlay-debug.js + OverlayManager-debug.js + KeyListener-debug.js + Tooltip-debug.js + Panel-debug.js + Dialog-debug.js + SimpleDialog-debug.js + ContainerEffect-debug.js /A container-debug.js
