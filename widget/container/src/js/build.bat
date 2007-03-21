5bdadfbbeb28123857144e35ba41b93ddd78f660
Todd Kloots <kloots@yahoo-inc.com>
2007-03-21 18:19:54 +0000

    Removed legacy built file

--- widget/container/src/js/build.bat
+++
@@ -1,3 +0,0 @@
-copy Config.js + Module.js + Overlay.js + OverlayManager.js + KeyListener.js + ContainerEffect.js /A container_core.js
-
-copy Config.js + Module.js + Overlay.js + OverlayManager.js + KeyListener.js + Tooltip.js + Panel.js + Dialog.js + SimpleDialog.js + ContainerEffect.js /A container.js
