42ee11ef75c84303498ec77bdc30067a0d951eb0
Jenny Han-Donnelly <jennyhan@yahoo-inc.com>
2007-04-05 23:00:22 +0000

    Added a Data section to all impl examples.

--- widget/datatable/examples/js/data.js
+++ widget/datatable/examples/js/data.js
@@ -20,27 +20,27 @@ YAHOO.example.Data = {
     },
 
     inventory: [
-        {SKU:"23-23874",Quantity:43,Item:"Helmet",Description:"Red baseball helmet. Size: Large."},
-        {SKU:"48-38835",Quantity:84,Item:"Football",Description:"Leather football."},
-        {SKU:"84-84848",Quantity:31,Item:"Goggles",Description:"Light blue swim goggles"},
-        {SKU:"84-84843",Quantity:56,Item:"Badminton Set",Description:"Set of 2 badminton rackets, net, and 3 birdies."},
-        {SKU:"84-39321",Quantity:128,Item:"Tennis Balls",Description:"Canister of 3 tennis balls."},
-        {SKU:"39-48949",Quantity:55,Item:"Snowboard",Description:""},
-        {SKU:"99-28128",Quantity:77,Item:"Cleats",Description:"Soccer cleats. Size: 10."},
-        {SKU:"83-48281",Quantity:65,Item:"Volleyball",Description:""},
-        {SKU:"89-32811",Quantity:67,Item:"Sweatband",Description:"Blue sweatband. Size: Medium."},
-        {SKU:"28-22847",Quantity:43,Item:"Golf Set",Description:"Set of 9 golf clubs and bag."},
-        {SKU:"38-38281",Quantity:35,Item:"Basketball Shorts",Description:"Green basketball shorts. Size: Small."},
-        {SKU:"82-38333",Quantity:288,Item:"Lip balm",Description:"Lip balm. Flavor: Cherry."},
-        {SKU:"21-38485",Quantity:177,Item:"Ping Pong Ball",Description:""},
-        {SKU:"83-38285",Quantity:87,Item:"Hockey Puck",Description:"Glow-in-the-dark hockey puck."}
+        {SKU:"23-23874", Quantity:43, Item:"Helmet", Description:"Red baseball helmet. Size: Large."},
+        {SKU:"48-38835", Quantity:84, Item:"Football", Description:"Leather football."},
+        {SKU:"84-84848", Quantity:31, Item:"Goggles", Description:"Light blue swim goggles"},
+        {SKU:"84-84843", Quantity:56, Item:"Badminton Set", Description:"Set of 2 badminton rackets, net, and 3 birdies."},
+        {SKU:"84-39321", Quantity:128, Item:"Tennis Balls", Description:"Canister of 3 tennis balls."},
+        {SKU:"39-48949", Quantity:55, Item:"Snowboard", Description:""},
+        {SKU:"99-28128", Quantity:77, Item:"Cleats", Description:"Soccer cleats. Size: 10."},
+        {SKU:"83-48281", Quantity:65, Item:"Volleyball", Description:""},
+        {SKU:"89-32811", Quantity:67, Item:"Sweatband", Description:"Blue sweatband. Size: Medium."},
+        {SKU:"28-22847", Quantity:43, Item:"Golf Set", Description:"Set of 9 golf clubs and bag."},
+        {SKU:"38-38281", Quantity:35, Item:"Basketball Shorts", Description:"Green basketball shorts. Size: Small."},
+        {SKU:"82-38333", Quantity:288, Item:"Lip balm", Description:"Lip balm. Flavor: Cherry."},
+        {SKU:"21-38485", Quantity:177, Item:"Ping Pong Ball", Description:""},
+        {SKU:"83-38285", Quantity:87, Item:"Hockey Puck", Description:"Glow-in-the-dark hockey puck."}
     ],
 
     bookorders: [
-        {id: "po-0167", date:new Date(1980, 2, 24), quantity:1, amount:4, title:"A Book About Nothing"},
-        {id: "po-0297", date:new Date("January 3, 1983"), quantity:null, amount:12.12345, title:"The Meaning of Life"},
-        {id: "po-0783", date:new Date(1978, 11, 12), quantity:12, amount:1.25, title:"This Book Was Meant to Be Read Aloud"},
-        {id: "po-1482", date:new Date("March 11, 1985"), quantity:6, amount:3.5, title:"Read Me Twice"}
+        {id:"po-0167", date:new Date(1980, 2, 24), quantity:1, amount:4, title:"A Book About Nothing"},
+        {id:"po-0297", date:new Date("January 3, 1983"), quantity:null, amount:12.12345, title:"The Meaning of Life"},
+        {id:"po-0783", date:new Date(1978, 11, 12), quantity:12, amount:1.25, title:"This Book Was Meant to Be Read Aloud"},
+        {id:"po-1482", date:new Date("March 11, 1985"), quantity:6, amount:3.5, title:"Read Me Twice"}
     ],
     
     areacodes: [
