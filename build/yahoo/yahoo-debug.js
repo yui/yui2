266d72bc3209f069b3cbf7f61e6139f25cedfc68
Adam Moore <adamoore@yahoo-inc.com>
2007-06-01 00:03:57 +0000

    replaced the old substitute and dump code with the code that works

--- build/yahoo/yahoo-debug.js
+++ build/yahoo/yahoo-debug.js
@@ -565,76 +565,7 @@ return (o && (typeof o === 'object' || YAHOO.lang.isFunction(o))) || false;
         YAHOO.lang.augmentObject.apply(this, a);
     },
 
-    /**
-     * Does variable substitution on a string. It scans through the string 
-     * looking for expressions enclosed in { } braces. If an expression 
-     * is found, it is used a key on the object.  If there is a space in
-     * the key, the first word is used for the key and the rest is provided
-     * to an optional function to be used to programatically determine the
-     * value (the extra information might be used for this decision). If 
-     * the value for the key in the object, or what is returned from the
-     * function has a string value, number value, or object value, it is 
-     * substituted for the bracket expression and it repeats.  If this
-     * value is an object, it uses the Object's toString() if this has
-     * been overridden, otherwise it does a shallow dump of the key/value
-     * pairs.
-     * @method substitute
-     * @param s {String} The string that will be modified.
-     * @param o {Object} An object containing the replacement values
-     * @param f {Function} An optional function that can be used to
-     *                     process each match.  It receives the key,
-     *                     value, and any extra metadata included with
-     *                     the key inside of the braces.
-     * @return {String} the substituted string
-     */
-    substitute: function (s, o, f) {
-        var i, j, k, key, v, meta, l=YAHOO.lang;
-        for (;;) {
-            i = s.lastIndexOf('{');
-            if (i < 0) {
-                break;
-            }
-            j = s.indexOf('}', i);
-            if (i + 1 >= j) {
-                break;
-            }
-
-            key=s.substring(i + 1, j);
-            meta=null;
-
-            k = key.indexOf(" ");
-            if (k > -1) {
-                meta = key.substring(k + 1);
-                key = key.substring(0, k);
-            }
-
-            v = o[key];
-            if (f) {
-                v = f(key, v, meta);
-            }
-
-            if (l.isObject(v)) {
-                if (l.isFunction(v)) {
-                    break;
-                } else if (l.isArray(v)) {
-                    v = l.dump(v, parseInt(meta, 10));
-                } else {
-                    if (v.toString === Object.prototype.toString) {
-                        v = l.dump(v, parseInt(meta, 10));
-                    } else {
-                        v = v.toString();
-                    }
-                }
-            } else if (!l.isString(v) && !l.isNumber(v)) {
-                break;
-            }
-
-            s = s.substring(0, i) + v + s.substring(j + 1);
-        }
-
-        return s;
-    },
-
+      
     /**
      * Returns a simple string representation of the object or array.
      * Other types of objects will be returned unprocessed.  Arrays
@@ -646,10 +577,12 @@ return (o && (typeof o === 'object' || YAHOO.lang.isFunction(o))) || false;
      * @return {String} the dump result
      */
     dump: function(o, d) {
-        var l=YAHOO.lang,i,len,s=[],OBJ="{...}";
+        var l=YAHOO.lang,i,len,s=[],OBJ="{...}",FUN="f(){...}";
 
-        if (!l.isObject(o) || l.isFunction(o)) {
+        if (!l.isObject(o) || o instanceof Date) {
             return o;
+        } else if  (l.isFunction(o)) {
+            return FUN;
         }
 
         d = (l.isNumber(d)) ? d : 3;
@@ -688,6 +621,82 @@ return (o && (typeof o === 'object' || YAHOO.lang.isFunction(o))) || false;
         }
 
         return s.join("");
+    },
+
+    /**
+     * Does variable substitution on a string. It scans through the string 
+     * looking for expressions enclosed in { } braces. If an expression 
+     * is found, it is used a key on the object.  If there is a space in
+     * the key, the first word is used for the key and the rest is provided
+     * to an optional function to be used to programatically determine the
+     * value (the extra information might be used for this decision). If 
+     * the value for the key in the object, or what is returned from the
+     * function has a string value, number value, or object value, it is 
+     * substituted for the bracket expression and it repeats.  If this
+     * value is an object, it uses the Object's toString() if this has
+     * been overridden, otherwise it does a shallow dump of the key/value
+     * pairs.
+     * @method substitute
+     * @param s {String} The string that will be modified.
+     * @param o {Object} An object containing the replacement values
+     * @param f {Function} An optional function that can be used to
+     *                     process each match.  It receives the key,
+     *                     value, and any extra metadata included with
+     *                     the key inside of the braces.
+     * @return {String} the substituted string
+     */
+    substitute: function (s, o, f) {
+        var i, j, k, key, v, meta, l=YAHOO.lang;
+        for (;;) {
+            i = s.lastIndexOf('{');
+            if (i < 0) {
+                break;
+            }
+            j = s.indexOf('}', i);
+            if (i + 1 >= j) {
+                break;
+            }
+
+            key = s.substring(i + 1, j);
+            meta = null;
+
+            k = key.indexOf(" ");
+            if (k > -1) {
+                meta = key.substring(k + 1);
+                key = key.substring(0, k);
+            }
+
+            v = o[key];
+            if (f) {
+                v = f(key, v, meta);
+            }
+
+            if (l.isObject(v)) {
+                //if (l.isFunction(v)) {
+                //    break;
+                //} else if (l.isArray(v)) {
+                if (l.isArray(v)) {
+                    v = l.dump(v, parseInt(meta, 10));
+                } else {
+                    meta = meta || "";
+                    var dump = meta.indexOf("dump");
+                    if (dump > -1) {
+                        meta = meta.substring(4);
+                    }
+                    if (v.toString === Object.prototype.toString || dump > -1) {
+                        v = l.dump(v, parseInt(meta, 10));
+                    } else {
+                        v = v.toString();
+                    }
+                }
+            } else if (!l.isString(v) && !l.isNumber(v)) {
+                break;
+            }
+
+            s = s.substring(0, i) + v + s.substring(j + 1);
+        }
+
+        return s;
     }
 
 };
