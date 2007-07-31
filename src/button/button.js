94d2d5b53830dfef184f76f88bc5d63ada5daa80
Todd Kloots <kloots@yahoo-inc.com>
2007-07-31 01:04:07 +0000

    *** empty log message ***

--- src/button/button.js
+++ src/button/button.js
@@ -294,7 +294,6 @@
             
             }
 
-            p_oElement.removeAttribute("name");
             p_oElement.removeAttribute("value");
 
             p_oElement.setAttribute("type", "button");
@@ -304,7 +303,8 @@
         }
 
         p_oElement.removeAttribute("id");
-
+        p_oElement.removeAttribute("name");
+        
         if ( !("tabindex" in p_oAttributes) ) {
 
             p_oAttributes.tabindex = p_oElement.tabIndex;
