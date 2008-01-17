9693c7f41a1ab9f9a9a00d27e2d090365ee5d535
Todd Kloots <kloots@yahoo-inc.com>
2008-01-17 23:51:06 +0000

    Updated the "maxheight" configuration property so that it also gives the Menu a width in IE. [fix bug 1652833]

--- widget/menu/src/js/menu.js
+++ widget/menu/src/js/menu.js
@@ -3568,7 +3568,8 @@ configMaxHeight: function (p_sType, p_aArgs, p_oMenu) {
         fnMouseOut = this._onScrollTargetMouseOut,
         nMinScrollHeight = this.cfg.getProperty("minscrollheight"),
         nHeight,
-        nOffsetWidth;
+        nOffsetWidth,
+        sWidth;
 
 
     if (nMaxHeight !== 0 && nMaxHeight < nMinScrollHeight) {
@@ -3604,24 +3605,36 @@ configMaxHeight: function (p_sType, p_aArgs, p_oMenu) {
         offsetParent's "position" property is also set to "absolute."  It is 
         possible to work around this bug by specifying a value for the width 
         property in addition to overflow.
+
+		In IE it is also necessary to give the Menu a width when the scrollbars are 
+		rendered to prevent the Menu from rendering with a width that is 100% of
+		the browser viewport.
     */
 
-    if (UA.gecko && this.parent && this.parent.parent && 
-        this.parent.parent.cfg.getProperty("position") == "dynamic" && 
-        !this.cfg.getProperty("width")) {
+	var bSetWidth = ((UA.gecko && this.parent && this.parent.parent && 
+        this.parent.parent.cfg.getProperty("position") == "dynamic") || UA.ie);
 
-        nOffsetWidth = oElement.offsetWidth;
 
-        /*
-            Measuring the difference of the offsetWidth before and after
-            setting the "width" style attribute allows us to compute the 
-            about of padding and borders applied to the element, which in 
-            turn allows us to set the "width" property correctly.
-        */
-        
-        oElement.style.width = nOffsetWidth + "px";
-        oElement.style.width = 
-                (nOffsetWidth - (oElement.offsetWidth - nOffsetWidth)) + "px";
+    if (bSetWidth) {
+
+		if (!this.cfg.getProperty("width")) {
+
+			nOffsetWidth = oElement.offsetWidth;
+	
+			/*
+				Measuring the difference of the offsetWidth before and after
+				setting the "width" style attribute allows us to compute the 
+				about of padding and borders applied to the element, which in 
+				turn allows us to set the "width" property correctly.
+			*/
+			
+			oElement.style.width = nOffsetWidth + "px";
+	
+			sWidth = (nOffsetWidth - (oElement.offsetWidth - nOffsetWidth)) + "px";
+	
+			this.cfg.setProperty("width", sWidth);
+		
+		}
 
     }
 
@@ -3646,7 +3659,6 @@ configMaxHeight: function (p_sType, p_aArgs, p_oMenu) {
     nHeight = (nMaxHeight - (oHeader.offsetHeight + oHeader.offsetHeight));
 
 
-
     if (nHeight > 0 && (oBody.offsetHeight > nMaxHeight)) {
 
         Dom.addClass(oBody, "yui-menu-body-scrolled");
@@ -3662,6 +3674,13 @@ configMaxHeight: function (p_sType, p_aArgs, p_oMenu) {
 
     }
     else if (oHeader && oFooter) {
+
+		if (bSetWidth) {
+
+			this.cfg.setProperty("width", "");
+		
+		}
+
 
         this._enableScrollHeader();
         this._enableScrollFooter();
