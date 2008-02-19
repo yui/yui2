7d663529021ba37f4ab0b41f833d5a930a930868
Adam Moore <adamoore@yahoo-inc.com>
2008-02-19 23:35:38 +0000

    Fixed Safari 2.x external script insert

--- util/get/build/get-debug.js
+++ util/get/build/get-debug.js
@@ -365,7 +365,7 @@ YAHOO.util.Get = function() {
             if (type === "script") {
 
                 // Safari 3.x supports the load event for script nodes (DOM2)
-                if (ua.webkit > 419) {
+                if (ua.webkit >= 420) {
 
                     n.addEventListener("load", function() {
                         YAHOO.log(id + " DOM2 onload " + url, "info", "Get");
