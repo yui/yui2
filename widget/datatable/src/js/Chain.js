3e9920d304c6a7a935b3b2249993bc566a5ecf45
Luke Smith <lsmith@yahoo-inc.com>
2008-03-04 20:17:44 +0000

    Adding 'end' event to Chain.  Fired after last callback is executed.  Chain is now an EventProvider. For use in rendering chains to push _sync* until after chain execution. [bug 1766940]

--- widget/datatable/src/js/Chain.js
+++ widget/datatable/src/js/Chain.js
@@ -22,6 +22,13 @@ YAHOO.util.Chain = function () {
      * @private
      */
     this.q = [].slice.call(arguments);
+
+    /**
+     * Event fired when the callback queue is emptied via execution (not via
+     * a call to chain.stop().
+     * @event end
+     */
+    this.createEvent('end');
 };
 
 YAHOO.util.Chain.prototype = {
@@ -46,6 +53,7 @@ YAHOO.util.Chain.prototype = {
         // If there is no callback in the queue or the Chain is currently
         // in an execution mode, return
         if (!c || this.id) {
+            this.fireEvent('end');
             return this;
         }
 
@@ -149,3 +157,4 @@ YAHOO.util.Chain.prototype = {
         return this;
     }
 };
+YAHOO.lang.augmentProto(YAHOO.util.Chain,YAHOO.util.EventProvider);
