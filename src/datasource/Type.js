d65588cea91939e220b79a883a892c2b4ee0ffb4
Jenny Han-Donnelly <jennyhan@yahoo-inc.com>
2008-07-16 19:36:54 +0000

    Adding missing file.

---
+++ src/datasource/Type.js
@@ -0,0 +1,157 @@
+/****************************************************************************/
+/****************************************************************************/
+/****************************************************************************/
+
+/**
+ * The static Number class provides helper functions to deal with data of type
+ * Number.
+ *
+ * @namespace YAHOO.util
+ * @requires yahoo
+ * @class Number
+ * @static
+ */
+ YAHOO.util.Number = {
+ 
+     /**
+     * Takes a native JavaScript Number and formats to string for display to user.
+     *
+     * @method format
+     * @param nData {Number} Number.
+     * @param oConfig {Object} (Optional) Optional configuration values:
+     *  <dl>
+     *   <dt>prefix {String}</dd>
+     *   <dd>String prepended before each number, like a currency designator "$"</dd>
+     *   <dt>decimalPlaces {Number}</dd>
+     *   <dd>Number of decimal places to round.</dd>
+     *   <dt>decimalSeparator {String}</dd>
+     *   <dd>Decimal separator</dd>
+     *   <dt>thousandsSeparator {String}</dd>
+     *   <dd>Thousands separator</dd>
+     *   <dt>suffix {String}</dd>
+     *   <dd>String appended after each number, like " items" (note the space)</dd>
+     *  </dl>
+     * @return {String} Formatted number for display.
+     */
+    format : function(nData, oConfig) {
+        oConfig = oConfig || {};
+        
+        if(!YAHOO.lang.isNumber(nData)) {
+            nData *= 1;
+        }
+
+        if(YAHOO.lang.isNumber(nData)) {
+            var sOutput = nData + "";
+            var sDecimalSeparator = (oConfig.decimalSeparator) ? oConfig.decimalSeparator : ".";
+            var nDotIndex;
+
+            // Manage decimals
+            if(YAHOO.lang.isNumber(oConfig.decimalPlaces)) {
+                // Round to the correct decimal place
+                var nDecimalPlaces = oConfig.decimalPlaces;
+                var nDecimal = Math.pow(10, nDecimalPlaces);
+                sOutput = Math.round(nData*nDecimal)/nDecimal + "";
+                nDotIndex = sOutput.lastIndexOf(".");
+
+                if(nDecimalPlaces > 0) {
+                    // Add the decimal separator
+                    if(nDotIndex < 0) {
+                        sOutput += sDecimalSeparator;
+                        nDotIndex = sOutput.length-1;
+                    }
+                    // Replace the "."
+                    else if(sDecimalSeparator !== "."){
+                        sOutput = sOutput.replace(".",sDecimalSeparator);
+                    }
+                    // Add missing zeros
+                    while((sOutput.length - 1 - nDotIndex) < nDecimalPlaces) {
+                        sOutput += "0";
+                    }
+                }
+            }
+            
+            // Add the thousands separator
+            if(oConfig.thousandsSeparator) {
+                var sThousandsSeparator = oConfig.thousandsSeparator;
+                nDotIndex = sOutput.lastIndexOf(sDecimalSeparator);
+                nDotIndex = (nDotIndex > -1) ? nDotIndex : sOutput.length;
+                var sNewOutput = sOutput.substring(nDotIndex);
+                var nCount = -1;
+                for (var i=nDotIndex; i>0; i--) {
+                    nCount++;
+                    if ((nCount%3 === 0) && (i !== nDotIndex)) {
+                        sNewOutput = sThousandsSeparator + sNewOutput;
+                    }
+                    sNewOutput = sOutput.charAt(i-1) + sNewOutput;
+                }
+                sOutput = sNewOutput;
+            }
+
+            // Prepend prefix
+            sOutput = (oConfig.prefix) ? oConfig.prefix + sOutput : sOutput;
+
+            // Append suffix
+            sOutput = (oConfig.suffix) ? sOutput + oConfig.suffix : sOutput;
+
+            return sOutput;
+        }
+        // Still not a Number, just return unaltered
+        else {
+            return nData;
+        }
+    }
+ };
+
+
+
+/****************************************************************************/
+/****************************************************************************/
+/****************************************************************************/
+
+/**
+ * The static Date class provides helper functions to deal with data of type
+ * Number.
+ *
+ * @namespace YAHOO.util
+ * @requires yahoo
+ * @class Date
+ * @static
+ */
+ YAHOO.util.Date = {
+     /**
+     * Takes a native JavaScript Date and formats to string for display to user.
+     *
+     * @method format
+     * @param oDate {Date} Date.
+     * @param oConfig {Object} (Optional) Optional configuration values:
+     *  <dl>
+     *   <dt>format {String}</dd>
+     *   <dd>Currently only the following formats are supported:
+     *   "MM/DD/YYYY", "YYYY/MM/DD", or "DD/MM/YYYY"</dd>
+     *  </dl>
+     * @return {String} Formatted date for display.
+     */
+    format : function(oDate, oConfig) {
+        oConfig = oConfig || {};
+        
+        if(oDate instanceof Date) {
+            var format = oConfig.format || "MM/DD/YYYY";
+            var mm = oDate.getMonth()+1;
+            var dd = oDate.getDate();
+            var yyyy = oDate.getFullYear();
+            
+            switch(format) {
+                case "YYYY/MM/DD":
+                    return yyyy + "/" + mm +"/" + dd;
+                case "DD/MM/YYYY":
+                    return dd + "/" + mm + "/" + yyyy;
+                default: // "MM/DD/YYYY"
+                    return mm + "/" + dd + "/" + yyyy;
+            }
+        }
+        else {
+            return YAHOO.lang.isValue(oDate) ? oDate : "";
+        }
+    }
+ };
+ 
