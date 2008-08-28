29b2b2896ebffc503682fa162da73190c9837a19
Todd Kloots <kloots@yahoo-inc.com>
2008-08-28 05:05:20 +0000

    Updated with changes to removeListener

--- src/event/Event.js
+++ src/event/Event.js
@@ -226,6 +226,16 @@ if (!YAHOO.util.Event) {
             OVERRIDE: 6,
 
             /**
+             * The original capture parameter passed into _addListener
+             * @property CAPTURE
+             * @type int
+             * @static
+             * @final
+             */
+            CAPTURE: 7,
+
+
+            /**
              * addListener/removeListener can throw errors in unexpected scenarios.
              * These errors are suppressed, the method returns false, and this property
              * is set
@@ -451,7 +461,7 @@ if (!YAHOO.util.Event) {
              * @private
              * @static
              */
-            _addListener: function(el, sType, fn, obj, override, bCapture) {
+            _addListener: function(el, sType, fn, obj, override, capture) {
 
                 if (!fn || !fn.call) {
                     YAHOO.log(sType + " addListener failed, invalid callback", "error", "Event");
@@ -462,11 +472,12 @@ if (!YAHOO.util.Event) {
                 if ( this._isValidCollection(el)) {
                     var ok = true;
                     for (var i=0,len=el.length; i<len; ++i) {
-                        ok = this.on(el[i], 
+                        ok = this._addListener(el[i], 
                                        sType, 
                                        fn, 
                                        obj, 
-                                       override) && ok;
+                                       override, 
+                                       capture) && ok;
                     }
                     return ok;
 
@@ -484,7 +495,7 @@ if (!YAHOO.util.Event) {
                     } else {
                         // defer adding the event until the element is available
                         this.onAvailable(el, function() {
-                           YAHOO.util.Event.on(el, sType, fn, obj, override);
+                           YAHOO.util.Event._addListener(el, sType, fn, obj, override, capture);
                         });
 
                         return true;
@@ -504,7 +515,7 @@ if (!YAHOO.util.Event) {
                 // handles explicitly during our one unload event.
                 if ("unload" == sType && obj !== this) {
                     unloadListeners[unloadListeners.length] =
-                            [el, sType, fn, obj, override];
+                            [el, sType, fn, obj, override, capture];
                     return true;
                 }
 
@@ -529,7 +540,7 @@ if (!YAHOO.util.Event) {
                                 obj);
                     };
 
-                var li = [el, sType, fn, wrappedFn, scope, obj, override];
+                var li = [el, sType, fn, wrappedFn, scope, obj, override, capture];
                 var index = listeners.length;
                 // cache the listener so we can try to automatically unload
                 listeners[index] = li;
@@ -565,12 +576,12 @@ if (!YAHOO.util.Event) {
 
                 } else {
                     try {
-                        this._simpleAdd(el, sType, wrappedFn, bCapture);
+                        this._simpleAdd(el, sType, wrappedFn, capture);
                     } catch(ex) {
                         // handle an error trying to attach an event.  If it fails
                         // we need to clean up the cache
                         this.lastError = ex;
-                        this.removeListener(el, sType, fn);
+                        this._removeListener(el, sType, fn, capture);
                         return false;
                     }
                 }
@@ -636,7 +647,7 @@ if (!YAHOO.util.Event) {
             /**
              * Removes a focus event listener
              *
-             * @method removeListener
+             * @method removeFocusListener
              *
              * @param {String|HTMLElement|Array|NodeList} el An id, an element 
              *  reference, or a collection of ids and/or elements to remove
@@ -649,7 +660,7 @@ if (!YAHOO.util.Event) {
              * @static
              */
             removeFocusListener: function (el, fn) { 
-                return this.removeListener(el, _FOCUS, fn);
+                return this._removeListener(el, _FOCUS, fn, true);
             },
 
             /**
@@ -681,7 +692,7 @@ if (!YAHOO.util.Event) {
             /**
              * Removes a blur event listener
              *
-             * @method removeListener
+             * @method removeBlurListener
              *
              * @param {String|HTMLElement|Array|NodeList} el An id, an element 
              *  reference, or a collection of ids and/or elements to remove
@@ -695,7 +706,7 @@ if (!YAHOO.util.Event) {
              */
             removeBlurListener: function (el, fn) { 
             
-                return this.removeListener(el, _BLUR, fn);
+                return this._removeListener(el, _BLUR, fn, true);
             
             },
 
@@ -764,7 +775,7 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
             /**
              * Removes an event listener
              *
-             * @method removeListener
+             * @method _removeListener
              *
              * @param {String|HTMLElement|Array|NodeList} el An id, an element 
              *  reference, or a collection of ids and/or elements to remove
@@ -773,11 +784,13 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
              * @param {Function} fn the method the event invokes.  If fn is
              *  undefined, then all event handlers for the type of event are 
              *  removed.
+             * @param {boolen}      capture capture or bubble phase             
              * @return {boolean} true if the unbind was successful, false 
              *  otherwise.
              * @static
+             * @private
              */
-            removeListener: function(el, sType, fn) {
+            _removeListener: function(el, sType, fn, capture) {
                 var i, len, li;
 
                 // The el argument can be a string
@@ -787,7 +800,7 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
                 } else if ( this._isValidCollection(el)) {
                     var ok = true;
                     for (i=el.length-1; i>-1; i--) {
-                        ok = ( this.removeListener(el[i], sType, fn) && ok );
+                        ok = ( this._removeListener(el[i], sType, fn, capture) && ok );
                     }
                     return ok;
                 }
@@ -820,7 +833,7 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
                 // The index is a hidden parameter; needed to remove it from
                 // the method signature because it was tempting users to
                 // try and take advantage of it, which is not possible.
-                var index = arguments[3];
+                var index = arguments[4];
   
                 if ("undefined" === typeof index) {
                     index = this._getCacheIndex(el, sType, fn);
@@ -857,7 +870,7 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
 
                 } else {
                     try {
-                        this._simpleRemove(el, sType, cacheItem[this.WFN], false);
+                        this._simpleRemove(el, sType, cacheItem[this.WFN], capture);
                     } catch(ex) {
                         this.lastError = ex;
                         return false;
@@ -873,6 +886,30 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
                 return true;
 
             },
+
+
+            /**
+             * Removes an event listener
+             *
+             * @method removeListener
+             *
+             * @param {String|HTMLElement|Array|NodeList} el An id, an element 
+             *  reference, or a collection of ids and/or elements to remove
+             *  the listener from.
+             * @param {String} sType the type of event to remove.
+             * @param {Function} fn the method the event invokes.  If fn is
+             *  undefined, then all event handlers for the type of event are 
+             *  removed.
+             * @return {boolean} true if the unbind was successful, false 
+             *  otherwise.
+             * @static
+             */
+            removeListener: function(el, sType, fn) {
+
+				return this._removeListener(el, sType, fn, false);
+
+            },
+
 
             /**
              * Returns the event's target element.  Safari sometimes provides
@@ -1367,7 +1404,7 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
                 if (elListeners) {
                     for (i=elListeners.length-1; i>-1; i--) {
                         var l = elListeners[i];
-                        this.removeListener(oEl, l.type, l.fn);
+                        this._removeListener(oEl, l.type, l.fn, l.capture);
                     }
                 }
 
@@ -1391,6 +1428,7 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
              * &nbsp;&nbsp;obj:    (object)   the custom object supplied to addListener
              * &nbsp;&nbsp;adjust: (boolean|object)  whether or not to adjust the default scope
              * &nbsp;&nbsp;scope: (boolean)  the derived scope based on the adjust parameter
+             * &nbsp;&nbsp;scope: (capture)  the capture parameter supplied to addListener
              * &nbsp;&nbsp;index:  (int)      its position in the Event util listener cache
              * @static
              */           
@@ -1419,6 +1457,7 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
                                     obj:    l[this.OBJ],
                                     adjust: l[this.OVERRIDE],
                                     scope:  l[this.ADJ_SCOPE],
+                                    capture:  l[this.CAPTURE],                                    
                                     index:  i
                                 });
                             }
@@ -1473,7 +1512,7 @@ return (this.webkit && this.webkit < 419 && ("click"==sType || "dblclick"==sType
                     for (j=listeners.length-1; j>-1; j--) {
                         l = listeners[j];
                         if (l) {
-                            EU.removeListener(l[EU.EL], l[EU.TYPE], l[EU.FN], j);
+                            EU._removeListener(l[EU.EL], l[EU.TYPE], l[EU.FN], l[EU.CAPTURE], j);
                         } 
                     }
                     l=null;
