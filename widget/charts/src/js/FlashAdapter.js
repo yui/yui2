abcc395bc104e45cf923266f78af72d223bb3ffc
Tripp Bridges <trippb@yahoo-inc.com>
2008-09-11 01:10:20 +0000

    Moved this.fireEvent("contentReady") due to timing issues for #2131929

--- widget/charts/src/js/FlashAdapter.js
+++ widget/charts/src/js/FlashAdapter.js
@@ -191,6 +191,7 @@ YAHOO.extend(YAHOO.widget.FlashAdapter, YAHOO.util.AttributeProvider,
 		{
 			case "swfReady":
    				this._loadHandler();
+				this.fireEvent("contentReady");
 				return;
 			case "log":
 				YAHOO.log(event.message, event.category, this.toString());
@@ -211,10 +212,8 @@ YAHOO.extend(YAHOO.widget.FlashAdapter, YAHOO.util.AttributeProvider,
 	{
 		this._initialized = false;
 		this._initAttributes(this._attributes);
-		this.setAttributes(this._attributes, true);
-		
+		this.setAttributes(this._attributes, true);		
 		this._initialized = true;
-		this.fireEvent("contentReady");
 	},
 	
 	set: function(name, value)
