e3408d0c35fdd69d95990def19ff63a67b29c1f8
Luke Smith <lsmith@yahoo-inc.com>
2008-10-06 17:15:28 +0000

    Changing setCSS to take obj lit and convert to cssText

--- util/stylesheet/src/js/StyleSheet.js
+++ util/stylesheet/src/js/StyleSheet.js
@@ -1,3 +1,205 @@
+(function () {
+
+var s      = document.creatElement('p').style,
+    u      = YAHOO.util,
+    lang   = YAHOO.lang,
+    sheets = {},
+    ssId   = 0,
+    floatAttr = ('cssFloat' in s) ? 'cssFloat' : 'styleFloat',
+    _toCssText, toCssText;
+
+_toCssText = function (css,base) {
+    var f = css.styleFloat || css.cssFloat || css['float'];
+    s.cssText = base || '';
+    if (f && !css[floatAttr]) {
+        css = lang.merge(css);
+        delete css.styleFloat; delete css.cssFloat; delete css['float'];
+        css[floatAttr] = f;
+    }
+    for (var prop in css) {
+        if (css.hasOwnProperty(prop)) {
+            s[prop] = css[prop];
+        }
+    }
+    return s.cssText;
+};
+
+toCssText = ('opacity' in s) ? _toCssText :
+    function (css, cssText) {
+        if ('opacity' in css) {
+            css = lang.merge(css,{
+                    filter: 'alpha(opacity='+(css.opacity*100)+')'
+                  });
+            delete css.opacity
+        }
+        return _toCssText(css,cssText);
+    };
+
+u.StyleSheet = function (node) {
+    var head,
+        sheet,
+        cssRules = {},
+        _rules,
+        _insertRule,
+        _deleteRule;
+
+    head = document.getElementsByTagName('head')[0];
+    if (!head) {
+        // TODO: do something. Preferably something smart
+        YAHOO.log('HEAD element not found to append STYLE node','error','YAHOO.util.StyleSheet');
+        throw new Error('HEAD element not found to append STYLE node');
+    }
+
+    // Return cached sheet if possible
+    if (node && sheets[node]) {
+        return sheets[node];
+    }
+
+    // If an id or style node is provided, use that node
+    // Use of link elements is still subject to same-domain restrictions
+    node = node && u.Dom.get(node);
+
+    // Return a cached sheet if possible
+    if (node && node.yuiSSID && sheets[node.yuiSSID]) {
+        return sheets[node.yuiSSID];
+    }
+
+    // Otherwise, create a style node and append to head
+    if (!node || node.parentNode !== head || (!node.sheet && !node.styleSheet)) {
+        // Generate the style node
+        node = document.createElement('style');
+        node.type = 'text/css';
+
+        // styleSheet isn't available on the style node in FF2 until appended
+        // to the head element.  style nodes appended to body do not affect
+        // change in Safari.
+        head.appendChild(node);
+    }
+
+    // IE stores StyleSheet under the "styleSheet" property
+    sheet = node.sheet || node.styleSheet;
+
+    // IE stores the rules collection under the "rules" property
+    _rules = ('cssRules' in sheet) ? 'cssRules' : 'rules';
+
+    // IE supports removeRule
+    _deleteRule = ('deleteRule' in sheet) ?
+        function (i) { sheet.deleteRule(i); } :
+        function (i) { sheet.removeRule(i); };
+
+    // IE supports addRule with different signature
+    _insertRule = ('insertRule' in sheet) ?
+        function (sel,o,i) {
+            sheet.insertRule(sel+' '+u.StyleSheet.toCssText(o),i);
+        } :
+        function (sel,o,i) {
+            sheet.addRule(sel,u.StyleSheet.toCssText(o),i);
+        };
+
+    // Initialize the cssRules map from the node
+    var i,r,sel;
+    for (i = sheet[_rules].length - 1; i >= 0; --i) {
+        r   = sheet[_rules][i];
+        sel = r.selectorText;
+
+        if (cssRules[sel]) {
+            cssRules[sel].style.cssText += ';' + r.style.cssText;
+            _deleteRule(i);
+        } else {
+            cssRules[sel] = r;
+        }
+    }
+
+    // Cache the sheet by the generated Id
+    node.yuiSSID = 'yui-stylesheet-' + ssId;
+    sheets[node.yuiSSID] = this;
+    ssId++;
+
+    // Public API
+    YAHOO.lang.augmentObject(this,{
+        // Enabling/disabling the stylesheet.  Changes may be made to rules
+        // while disabled.
+        enable : function () { sheet.disabled = false; },
+
+        disable : function () { sheet.disabled = true; },
+
+        isEnabled : function () { return !sheet.disabled; },
+
+        // Update cssText for a rule.  Add the rule if it's not present already
+        setCSS : function (sel,css) {
+            var rule    = cssRules[sel],
+                idx;
+
+            // Opera throws an error if there's a syntax error in the cssText.
+            // Should I do something about it, or let the error happen?
+            if (rule) {
+                rule.style.cssText = u.StyleSheet.toCssText(css,rule.style.cssText);
+            } else {
+                idx = sheet[_rules].length;
+                _insertRule(sel,'{'+u.StyleSheet.toCssText(css)+'}',idx);
+
+                // Safari replaces the rules collection, but maintains the rule
+                // instances in the new collection when rules are added/removed
+                cssRules[sel] = sheet[_rules][idx];
+            }
+        },
+
+        // remove rule properties or an entire rule
+        unsetCSS : function (sel,css) {
+            var rule = cssRules[sel],
+                rules, i, cssText;
+
+            if (rule) {
+                if (!css) { // remove the rule altogether
+                    rules = sheet[_rules];
+                    for (i = rules.length - 1; i >= 0; --i) {
+                        if (rules[i] === rule) {
+                            delete cssRules[sel];
+                            _deleteRule(i);
+                            break;
+                        }
+                    }
+                    return;
+                }
+
+                if (!YAHOO.lang.isArray(css)) {
+                    css = [css];
+                }
+
+                cssText = rule.style.cssText;
+                for (i = css.length - 1; i >= 0; --i) {
+                    // regex strip properties and any subproperties (e.g. 'font'
+                    // also strips line-height, font-weight, font-family etc)
+                    cssText = cssText.replace(
+                        YAHOO.util.StyleSheet._propertyRE(css[i]),'');
+                }
+
+                rule.style.cssText = cssText;
+            }
+        }
+    },true);
+
+};
+
+YAHOO.lang.augmentObject(u.StyleSheet, {
+    // Regex collection for unsetting rule properties.  Initialized with the
+    // properties that have child properties.  Added to from _propertyRE(prop).
+    PROP_REGEX : {
+        'border' : /border(?:-(?:(?:top|right|bottom|left)(?:-(?:color|style|width))?|(?:color|style|width)))?\s*:[^;]*(?:;|$)/gi,
+        'font' : /(?:font(?:-(?:size|family|style|variant|weight))?|line-height)\s*:[^;]*(?:;|$)/gi
+    },
+
+    // Memoized regex generator to remove properties from cssText
+    _propertyRE : function (prop) {
+        return this.PROP_REGEX[prop] ||
+              (this.PROP_REGEX[prop] = new RegExp(prop+'(?:-[a-z\\-]+)?\\s*:[^;]*(?:;|$)','gi'));
+    },
+
+    toCssText : toCssText
+},true);
+
+})();
+
 /*
 
 NOTES
@@ -52,157 +254,3 @@ NOTES
    ??? - unsetting font-size-adjust has the same effect as unsetting font-size
 */
 
-(function () {
-
-YAHOO.util.StyleSheet = function (node) {
-    var head,
-        sheet,
-        cssRules = {},
-        _rules,
-        _insertRule,
-        _deleteRule;
-
-    head = document.getElementsByTagName('head')[0];
-    if (!head) {
-        // TODO: do something. Preferably something smart
-        YAHOO.log('HEAD element not found to append STYLE node','error','YAHOO.util.StyleSheet');
-        throw new Error('HEAD element not found to append STYLE node');
-    }
-
-    // If an id or style node is provided, use that node
-    // Us of link elements is still subject to same-domain restrictions
-    node = node && YAHOO.util.Dom.get(node);
-
-    // Otherwise, create a style node and append to head
-    if (!node || node.parentNode !== head && (!node.sheet && !node.styleSheet)) {
-        // Generate the style node
-        node = document.createElement('style');
-        node.type = 'text/css';
-
-        // styleSheet isn't available on the style node in FF2 until appended
-        // to the head element.  style nodes appended to body do not affect
-        // change in Safari.
-        head.appendChild(node);
-    }
-
-    // IE stores StyleSheet under the "styleSheet" property
-    sheet = node.sheet || node.styleSheet;
-
-    // IE stores the rules collection under the "rules" property
-    _rules = ('cssRules' in sheet) ? 'cssRules' : 'rules';
-
-    // IE supports removeRule
-    _deleteRule = ('deleteRule' in sheet) ?
-        function (i) { sheet.deleteRule(i); } :
-        function (i) { sheet.removeRule(i); };
-
-    // IE supports addRule with different signature
-    _insertRule = ('insertRule' in sheet) ?
-        function (sel,cssText,i) { sheet.insertRule(sel+' '+cssText,i); } :
-        function (sel,cssText,i) { sheet.addRule(sel,cssText,i); };
-
-    // Initialize the cssRules map from the node
-    var i,r,sel;
-    for (i = sheet[_rules].length - 1; i >= 0; --i) {
-        r   = sheet[_rules][i];
-        sel = r.selectorText;
-
-        if (cssRules[sel]) {
-            cssRules[sel].style.cssText += ';' + r.style.cssText;
-            _deleteRule(i);
-        } else {
-            cssRules[sel] = r;
-        }
-    }
-
-    // Public API
-    YAHOO.lang.augmentObject(this,{
-        // Enabling/disabling the stylesheet.  Changes may be made to rules
-        // while disabled.
-        enable : function () { sheet.disabled = false; },
-
-        disable : function () { sheet.disabled = true; },
-
-        isEnabled : function () { return !sheet.disabled; },
-
-        // Normalize opacity assignment and clearing
-        setOpacity : (('opacity' in node.style) ?
-            function (sel,op) { this.setCSS(sel,'opacity:'+op); } :
-            function (sel,op) {
-                this.setCSS(sel,'filter: alpha(opacity='+(op*100)+')');
-            }),
-
-        unsetOpacity : (('opacity' in node.style) ?
-            function (sel) { this.unsetCSS(sel,'opacity'); } :
-            function (sel) { this.unsetCSS(sel,'filter'); }),
-
-        // Update cssText for a rule.  Add the rule if it's not present already
-        setCSS : function (sel,cssText) {
-            var rule = cssRules[sel];
-
-
-            // Opera throws an error if there's a syntax error in the cssText.
-            // Should I do something about it, or let the error happen?
-            if (rule) {
-                rule.style.cssText += ';'+cssText;
-            } else {
-                var idx = sheet[_rules].length;
-                _insertRule(sel,'{'+cssText+'}',idx);
-
-                // Safari replaces the rules collection, but maintains the rule
-                // instances in the new collection when rules are added/removed
-                cssRules[sel] = sheet[_rules][idx];
-            }
-        },
-
-        // remove rule properties or an entire rule
-        unsetCSS : function (sel,props) {
-            var rule = cssRules[sel],
-                rules, i, tmpCssText;
-
-            if (rule) {
-                if (!props) { // remove the rule altogether
-                    rules = sheet[_rules];
-                    for (i = rules.length - 1; i >= 0; --i) {
-                        if (rules[i] === rule) {
-                            delete cssRules[sel];
-                            _deleteRule(i);
-                            break;
-                        }
-                    }
-                    return;
-                }
-
-                if (!YAHOO.lang.isArray(props)) {
-                    props = [props];
-                }
-
-                tmpCssText = rule.style.cssText;
-                for (i = props.length - 1; i >= 0; --i) {
-                    // regex strip properties and any subproperties (e.g. 'font'
-                    // also strips line-height, font-weight, font-family etc)
-                    tmpCssText = tmpCssText.replace(
-                        YAHOO.util.StyleSheet._propertyRE(props[i]),'');
-                }
-
-                rule.style.cssText = tmpCssText;
-            }
-        }
-    },true);
-
-};
-
-// Regex collection for unsetting rule properties.  Initialized with the
-// properties that have child properties.  Added to from _propertyRE(prop).
-YAHOO.util.StyleSheet.PROP_REGEX = {
-    'border' : /border(?:-(?:(?:top|right|bottom|left)(?:-(?:color|style|width))?|(?:color|style|width)))?\s*:[^;]*(?:;|$)/gi,
-    'font' : /(?:font(?:-(?:size|family|style|variant|weight))?|line-height)\s*:[^;]*(?:;|$)/gi
-};
-
-// Memoized regex generator to remove properties from cssText
-YAHOO.util.StyleSheet._propertyRE = function (prop) {
-    return this.PROP_REGEX[prop] ||
-          (this.PROP_REGEX[prop] = new RegExp(prop+'(?:-[a-z\\-]+)?\\s*:[^;]*(?:;|$)','gi'));
-};
-
-})();
