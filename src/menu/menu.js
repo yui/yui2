23e45ff9cd61aebdc222cda8005c9ac108857d22
Todd Kloots <kloots@yahoo-inc.com>
2008-10-09 20:50:25 +0000

    Fixed typo [bug 2271933]

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -5517,7 +5517,7 @@ initDefaultConfig: function () {
     /**
     * @config keepopen
     * @description Boolean indicating if the menu should remain open when clicked.
-    * @default flase
+    * @default false
     * @type Boolean
     */
     oConfig.addProperty(
