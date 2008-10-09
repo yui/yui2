d3d3df5c50bd9a35d2ca0764f985b2ca0239ca5d
Luke Smith <lsmith@yahoo-inc.com>
2008-10-09 22:27:47 +0000

    Added ability to create an entire sheet sheet via constructor.  Tweaks and test coverage.  unsetCSS now uses style[prop] = '' for all but IE (no more regex)

--- util/stylesheet/src/js/StyleSheet.js
+++ util/stylesheet/src/js/StyleSheet.js
@@ -1,16 +1,19 @@
 (function () {
 
-var x      = document.createElement('p').style, // worker style collection
+var style  = document.createElement('p').style, // worker style collection
     u      = YAHOO.util,
     lang   = YAHOO.lang,
     sheets = {},
     ssId   = 0,
-    floatAttr = ('cssFloat' in x) ? 'cssFloat' : 'styleFloat',
-    _toCssText, toCssText;
+    floatAttr = ('cssFloat' in style) ? 'cssFloat' : 'styleFloat',
+    _toCssText, toCssText,
+    _unsetOpacity,
+    _unsetProperty,
+    _createSheet,factoryNode;
 
 _toCssText = function (css,base) {
     var f = css.styleFloat || css.cssFloat || css['float'];
-    x.cssText = base || '';
+    style.cssText = base || '';
     if (f && !css[floatAttr]) {
         css = lang.merge(css);
         delete css.styleFloat; delete css.cssFloat; delete css['float'];
@@ -18,17 +21,17 @@ _toCssText = function (css,base) {
     }
     for (var prop in css) {
         if (css.hasOwnProperty(prop)) {
-            x[prop] = css[prop];
+            style[prop] = css[prop];
         }
     }
-    return x.cssText;
+    return style.cssText;
 };
 
 // Wrap IE's toCssText to catch opacity.  The copy/merge is to preserve the
 // input object's integrity, but if float and opacity are set, the input will
 // be copied twice in IE.  Is there a way to avoid this without increasing the
 // byte count?
-toCssText = ('opacity' in x) ? _toCssText :
+toCssText = ('opacity' in style) ? _toCssText :
     function (css, cssText) {
         if ('opacity' in css) {
             css = lang.merge(css,{
@@ -39,10 +42,71 @@ toCssText = ('opacity' in x) ? _toCssText :
         return _toCssText(css,cssText);
     };
 
-u.StyleSheet = function (node) {
+
+_unsetOpacity = ('opacity' in style) ?
+    function (style) { style.opacity = ''; } :
+    function (style) { style.filter = ''; };
+        
+style.border = "1px solid red";
+style.border = ''; // IE doesn't unset child properties
+_unsetProperty = style.borderLeft ?
+    function (style,prop) {
+        var p;
+        if (prop !== floatAttr && prop.toLowerCase().indexOf('float') != -1) {
+            prop = floatAttr;
+        }
+        if (typeof style[prop] === 'string') {
+            switch (prop) {
+                case 'opacity':
+                case 'filter' : _unsetOpacity(style); break;
+                case 'font'   :
+                    style.font       = style.fontStyle = style.fontVariant =
+                    style.fontWeight = style.fontSize  = style.lineHeight  =
+                    style.fontFamily = '';
+                    break;
+                default       :
+                    for (p in style) {
+                        if (p.indexOf(prop) === 0) {
+                            style[p] = '';
+                        }
+                    }
+            }
+        }
+    } :
+    function (style,prop) {
+        if (prop !== floatAttr && prop.toLowerCase().indexOf('float') != -1) {
+            prop = floatAttr;
+        }
+        if (typeof style[prop] === 'string') {
+            if (prop === 'opacity') {
+                _unsetOpacity(style);
+            } else {
+                style[prop] = '';
+            }
+        }
+    };
+    
+factoryNode = document.createElement('p');
+factoryNode.innerHTML = '<style></style>';
+_createSheet = factoryNode.firstChild ?
+    function (cssText) {
+        factoryNode = document.createElement('p');
+        factoryNode.innerHTML = '<style type="text/css">'+cssText+'</style>';
+        return factoryNode.firstChild;
+    } :
+    function (cssText) {
+        factoryNode = document.createElement('style');
+        factoryNode.type = 'text/css';
+        if (factoryNode.styleSheet) {
+            factoryNode.styleSheet.cssText = cssText;
+        }
+        return factoryNode;
+    };
+
+u.StyleSheet = function (seed, name) {
     var head,
+        node,
         sheet,
-        name = node+'',
         cssRules = {},
         _rules,
         _insertRule,
@@ -50,7 +114,7 @@ u.StyleSheet = function (node) {
 
     // Factory or constructor
     if (!(this instanceof u.StyleSheet)) {
-        return new u.StyleSheet(node);
+        return new u.StyleSheet(seed,name);
     }
 
     head = document.getElementsByTagName('head')[0];
@@ -60,27 +124,27 @@ u.StyleSheet = function (node) {
         throw new Error('HEAD element not found to append STYLE node');
     }
 
-    // Return cached sheet if possible
-    if (node && sheets[node]) {
-        return sheets[node];
-    }
-
-    // If an id or style node is provided, use that node
-    // Use of link elements is still subject to same-domain restrictions
-    node = node && u.Dom.get(node);
-
-    // Return a cached sheet if possible
-    if (node && /^(?:style|link)$/i.test(node.nodeName) &&
-            node.yuiSSID && sheets[node.yuiSSID]) {
+    node = u.Dom.get(seed);
+    if (seed && sheets[seed]) {
+        return sheets[seed];
+    } else if (node && node.yuiSSID && sheets[node.yuiSSID]) {
         return sheets[node.yuiSSID];
     }
 
-    // Otherwise, create a style node and append to head
-    if (!node || node.parentNode !== head || (!node.sheet && !node.styleSheet)) {
-        // Generate the style node
+    if (typeof seed === 'string') {
+        if (seed.indexOf('{') != -1) { // create entire sheet from seed cssText
+            node = _createSheet(seed);
+        } else if (!name) {
+            name = seed;
+        }
+    }
+
+    if (!node || !/^(?:style|link)$/i.test(node.nodeName)) {
         node = document.createElement('style');
         node.type = 'text/css';
+    }
 
+    if (node.parentNode !== head) {
         // styleSheet isn't available on the style node in FF2 until appended
         // to the head element.  style nodes appended to body do not affect
         // change in Safari.
@@ -122,7 +186,9 @@ u.StyleSheet = function (node) {
     // Cache the sheet by the generated Id
     node.yuiSSID = 'yui-stylesheet-' + (ssId++);
     u.StyleSheet.register(node.yuiSSID,this);
-    u.StyleSheet.register(name,this);
+    if (name) {
+        u.StyleSheet.register(name,this);
+    }
 
     // Public API
     YAHOO.lang.augmentObject(this,{
@@ -130,9 +196,9 @@ u.StyleSheet = function (node) {
 
         // Enabling/disabling the stylesheet.  Changes may be made to rules
         // while disabled.
-        enable : function () { sheet.disabled = false; },
+        enable : function () { sheet.disabled = false; return this; },
 
-        disable : function () { sheet.disabled = true; },
+        disable : function () { sheet.disabled = true; return this; },
 
         isEnabled : function () { return !sheet.disabled; },
 
@@ -155,15 +221,34 @@ u.StyleSheet = function (node) {
                 // instances in the new collection when rules are added/removed
                 cssRules[sel] = sheet[_rules][idx];
             }
+            return this;
         },
 
         // remove rule properties or an entire rule
         unsetCSS : function (sel,css) {
             var rule = cssRules[sel],
-                rules, i, cssText;
+                remove = !css,
+                rules, i;
 
             if (rule) {
-                if (!css) { // remove the rule altogether
+                if (!remove) {
+                    if (!YAHOO.lang.isArray(css)) {
+                        css = [css];
+                    }
+
+                    style.cssText = rule.style.cssText;
+                    for (i = css.length - 1; i >= 0; --i) {
+                        _unsetProperty(style,css[i]);
+                    }
+
+                    if (style.cssText) {
+                        rule.style.cssText = style.cssText;
+                    } else {
+                        remove = true;
+                    }
+                }
+                
+                if (remove) { // remove the rule altogether
                     rules = sheet[_rules];
                     for (i = rules.length - 1; i >= 0; --i) {
                         if (rules[i] === rule) {
@@ -172,42 +257,15 @@ u.StyleSheet = function (node) {
                             break;
                         }
                     }
-                    return;
                 }
-
-                if (!YAHOO.lang.isArray(css)) {
-                    css = [css];
-                }
-
-                cssText = rule.style.cssText;
-                for (i = css.length - 1; i >= 0; --i) {
-                    // regex strip properties and any subproperties (e.g. 'font'
-                    // also strips line-height, font-weight, font-family etc)
-                    cssText = cssText.replace(
-                        YAHOO.util.StyleSheet._propertyRE(css[i]),'');
-                }
-
-                rule.style.cssText = cssText;
             }
+            return this;
         }
     },true);
 
 };
 
 YAHOO.lang.augmentObject(u.StyleSheet, {
-    // Regex collection for unsetting rule properties.  Initialized with the
-    // properties that have child properties.  Added to from _propertyRE(prop).
-    PROP_REGEX : {
-        'border' : /border(?:-(?:(?:top|right|bottom|left)(?:-(?:color|style|width))?|(?:color|style|width)))?\s*:[^;]*(?:;|$)/gi,
-        'font' : /(?:font(?:-(?:size|family|style|variant|weight))?|line-height)\s*:[^;]*(?:;|$)/gi
-    },
-
-    // Memoized regex generator to remove properties from cssText
-    _propertyRE : function (prop) {
-        return this.PROP_REGEX[prop] ||
-              (this.PROP_REGEX[prop] = new RegExp(prop.replace(/([A-Z])/g,'-$1')+'(?:-[a-z\\-]+)?\\s*:[^;]*(?:;|$)','gi'));
-    },
-
     toCssText : toCssText,
 
     register : function (name,sheet) {
@@ -247,8 +305,18 @@ NOTES
    Exception being Opera 9.27, where style.property is always faster than
    style.cssText.
  * Opera 9.5b throws a syntax error when assigning cssText with a syntax error.
+ * Opera 9.5 doesn't honor rule.style.cssText = ''.  Previous style persists.
+   You have to remove the rule altogether.
  * Stylesheet properties set with !important will trump inline style set on an
    element or in el.style.property.
+ * When creating an entire sheet at once, only FF2/3 & Opera allow creating a
+   style node, setting its innerHTML and appending to head.
+ * When creating an entire sheet at once, Safari requires the style node to be
+   created with content in innerHTML of another element.
+ * When creating an entire sheet at once, IE requires the style node content to
+   be set via node.styleSheet.cssText
+ * When creating an entire sheet at once in IE, styleSheet.cssText can't be
+   written until node.type = 'text/css'; is performed.
  * Setting complex properties in cssText will SOMETIMES allow child properties
    to be unset
    set         unset              FF2  FF3  S3.1  IE6  IE7  Op9.27  Op9.5
