74f91af90865055238de44bbebeac9a771bb4939
Dav Glass <davglass@gmail.com>
2009-01-05 12:33:19 -0800

    Initial Source Move

---
+++ src/resize/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#echo "Setup skinning"
+#cat ./src/css/skin-sam.css | sed -e 's/sprite\.png/..\/..\/..\/..\/assets\/skins\/sam\/sprite\.png/' > ./src/css/skins/sam/resize-skin.css
+#cp ./src/css/skin-sam.css ./src/css/skins/sam/resize-skin.css
+#wait
+
+echo "Running (ant all)..."
+wait
+ant all
+#wait
+#rm -rRf ./build
