3093407a125af548df4152acdb8fd7ce15db454a
Gopal Venkatesan <gopal_v@yahoo-inc.com>
2009-01-06 10:00:04 +0530

    Fixed the bug calculating width for vertical carousel.

--- build/carousel/carousel-beta.js
+++ build/carousel/carousel-beta.js
@@ -1849,7 +1849,7 @@
             }
 
             if (this.get("isVertical")) {
-                size = getCarouselItemSize.call(this);
+                size = getCarouselItemSize.call(this, "width");
                 size = size < config.MIN_WIDTH ? config.MIN_WIDTH : size;
                 this.setStyle("width",  size + "px");
                 this.addClass(cssClass.VERTICAL);
