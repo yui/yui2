d717bdb010b969a50870eb38b588190f46d94f3e
Jenny Han Donnelly <jennydonnelly@yahoo-inc.com>
2009-02-27 18:03:04 -0800

    JS Lint cleanups.

--- src/datasource/js/DataSource.js
+++ src/datasource/js/DataSource.js
@@ -891,9 +891,11 @@ handleResponse : function(oRequest, oRawResponse, oCallback, oCaller, tId) {
     this.fireEvent("responseEvent", {tId:tId, request:oRequest, response:oRawResponse,
             callback:oCallback, caller:oCaller});
     YAHOO.log("Received live data response for \"" + oRequest + "\"", "info", this.toString());
-    var xhr = (this.dataType == DS.TYPE_XHR) ? true : false;
-    var oParsedResponse = null;
-    var oFullResponse = oRawResponse;
+    
+    var xhr = (this.dataType == DS.TYPE_XHR) ? true : false,
+        oParsedResponse = null,
+        oFullResponse = oRawResponse,
+        parseArgs;
     
     // Try to sniff data type if it has not been defined
     if(this.responseType === DS.TYPE_UNKNOWN) {
@@ -938,7 +940,7 @@ handleResponse : function(oRequest, oRawResponse, oCallback, oCaller, tId) {
             try {
                 // Convert to JS array if it's a string
                 if(lang.isString(oFullResponse)) {
-                    var parseArgs = [oFullResponse].concat(this.parseJSONArgs);
+                    parseArgs = [oFullResponse].concat(this.parseJSONArgs);
                     // Check for YUI JSON Util
                     if(lang.JSON) {
                         oFullResponse = lang.JSON.parse.apply(lang.JSON,parseArgs);
@@ -986,7 +988,7 @@ Math.max(oFullResponse.lastIndexOf("]"),oFullResponse.lastIndexOf("}"));
             try {
                 // Convert to JSON object if it's a string
                 if(lang.isString(oFullResponse)) {
-                    var parseArgs = [oFullResponse].concat(this.parseJSONArgs);
+                    parseArgs = [oFullResponse].concat(this.parseJSONArgs);
                     // Check for YUI JSON Util
                     if(lang.JSON) {
                         oFullResponse = lang.JSON.parse.apply(lang.JSON,parseArgs);
