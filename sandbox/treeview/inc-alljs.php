45b3a2cb0667c19280685e4853184ae0148d1554
Adam Moore <adamoore@yahoo-inc.com>
2009-03-27 11:37:20 -0700

    The treeview animation classes were not being inserted into the build files for some reason -- restored.

--- sandbox/treeview/inc-alljs.php
+++ sandbox/treeview/inc-alljs.php
@@ -1,5 +1,7 @@
 <?php
-$ext = "";
+$ext = "debug";
+$mode = "debug";
+
 if (isset($_GET["mode"])) {
     $mode = $_GET["mode"];
 
@@ -10,44 +12,41 @@ if (isset($_GET["mode"])) {
     if ($mode == "debug") {
         $ext = "-debug";
     }
-
-} else {
-    $mode = "";
 }
 
 ?>
 <script type="text/javascript" src="../../build/yahoo/yahoo-min.js" ></script>
 <script type="text/javascript" src="../../build/event/event-min.js"></script>
+<script type="text/javascript" src="../../build/dom/dom-min.js"></script>
 
 <!-- The following are required for the logger -->
-<script type="text/javascript" src="../../build/dom/dom-min.js"></script>
 <script type="text/javascript" src="../../build/logger/logger-min.js"></script>
 <!-- End logger reqs -->
 
 <?php
-if (isset($_GET["mode"]) && $_GET["mode"] == "min") {
-	echo '<script type="text/javascript" src="../build/treeview-min.js" ></script>';
-} else if (isset($_GET["mode"]) && $_GET["mode"] == "build") {
+if ($mode == "min") {
+	echo '<script type="text/javascript" src="../../build/treeview-min.js" ></script>';
+} else if ($mode == "build") {
 	echo '<script type="text/javascript" src="../../build/treeview/treeview.js" ></script>';
-} else if (isset($_GET["mode"]) && $_GET["mode"] == "dist") {
+} else if ($mode == "debug") {
 	echo '<script type="text/javascript" src="../../build/treeview/treeview-debug.js" ></script>';
-} else if (isset($_GET["mode"]) && $_GET["mode"] == "prod") {
-	echo '<script type="text/javascript" src="http://us.js2.yimg.com/us.js.yimg.com/lib/common/widgets/2/treeview/treeview_2.0.0-b3.js" ></script>';
+} else if ($mode == "prod") {
+	echo '<script type="text/javascript" src="http://us.js2.yimg.com/us.js.yimg.com/lib/common/widgets/2/treeview/treeview_2.7.0.js" ></script>';
 } else {
 	echo '
-		<script type="text/javascript" src="../src/js/TreeView.js" ></script>
-		<script type="text/javascript" src="../src/js/Node.js" ></script>
-		<script type="text/javascript" src="../src/js/RootNode.js" ></script>
-		<script type="text/javascript" src="../src/js/TextNode.js" ></script>
-		<script type="text/javascript" src="../src/js/HTMLNode.js" ></script>
-		<script type="text/javascript" src="../src/js/MenuNode.js" ></script>
+		<script type="text/javascript" src="../../src/js/TreeView.js" ></script>
+		<script type="text/javascript" src="../../src/js/Node.js" ></script>
+		<script type="text/javascript" src="../../src/js/RootNode.js" ></script>
+		<script type="text/javascript" src="../../src/js/TextNode.js" ></script>
+		<script type="text/javascript" src="../../src/js/HTMLNode.js" ></script>
+		<script type="text/javascript" src="../../src/js/MenuNode.js" ></script>
         <!--
-		<script type="text/javascript" src="../src/js/ListNode.js" ></script>
+		<script type="text/javascript" src="../../../src/js/ListNode.js" ></script>
         -->
 
-		<script type="text/javascript" src="../src/js/anim/TVAnim.js" ></script>
-		<script type="text/javascript" src="../src/js/anim/TVFadeIn.js" ></script>
-		<script type="text/javascript" src="../src/js/anim/TVFadeOut.js" ></script>
+		<script type="text/javascript" src="../../src/js/anim/TVAnim.js" ></script>
+		<script type="text/javascript" src="../../src/js/anim/TVFadeIn.js" ></script>
+		<script type="text/javascript" src="../../src/js/anim/TVFadeOut.js" ></script>
 	';
 }
 ?>
