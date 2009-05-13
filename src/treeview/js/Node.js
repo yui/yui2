0390122f7fc4f61010cb5b6490794433d459ce90
Daniel Barreiro <satyam@satyam.com.ar>
2009-05-13 12:20:18 +0200

    The first patch assumed that Nodes would be created with highlighState in its default value, which might not be so.
    Now it forces the state from the ancestors, just as if the node had been
    there from the start except in the case of the ancestor being partially
    highlighted (meaning, some children highlighted, some not)
    in which case it will leave the highlightState of the
    dynamically loaded node as it was when created since it can't know
    whether the new node should be in the fully highlighted or not highlighted
    group.

--- src/treeview/js/Node.js
+++ src/treeview/js/Node.js
@@ -1030,10 +1030,16 @@ YAHOO.widget.Node.prototype = {
     loadComplete: function() {
         this.logger.log(this.index + " loadComplete, children: " + this.children.length);
         this.getChildrenEl().innerHTML = this.completeRender();
-		if (this.highlightState == 1 && !this.tree.singleNodeHighlight && this.propagateHighlightDown) {
-			for (var i = 0;i < this.children.length;i++) {
+		if (this.propagageHighlightDown) {
+			if (this.highlightState === 1 && !this.tree.singleNodeHighlight) {
+				for (var i = 0; i < this.children.length; i++) {
 				this.children[i].highlight(true);
 			}
+			} else if (this.highlightState === 0 || this.tree.singleNodeHighlight) {
+				for (i = 0; i < this.children.length; i++) {
+					this.children[i].unhighlight(true);
+				}
+			} // if (highlighState == 2) leave child nodes with whichever highlight state they are set
 		}
 				
         this.dynamicLoadComplete = true;
