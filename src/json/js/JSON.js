38a7480792bd6a87120f1624f3457e8addb0b9af
Luke Smith <lsmith@yahoo-inc.com>
2009-06-04 20:59:40 -0700

    Reverting last change.  Needless optimization.  Output unaffected.

--- src/json/js/JSON.js
+++ src/json/js/JSON.js
@@ -155,7 +155,7 @@ function _prepare(s) {
  * @static
  */
 function _isValid(str) {
-    return str && l.isString(str) &&
+    return l.isString(str) &&
             _INVALID.test(str.
             replace(_ESCAPES,'@').
             replace(_VALUES,']').
