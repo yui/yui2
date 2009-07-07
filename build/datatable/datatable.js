d5650a390fbd50ec447f73b6c06355f1575efe5c
Jenny Han Donnelly <jennydonnelly@yahoo-inc.com>
2009-07-07 15:44:23 -0700

    this.constructor.superclass -> YAHOO.widget.ColumnDD.superclass

--- build/datatable/datatable.js
+++ build/datatable/datatable.js
@@ -1371,7 +1371,7 @@ if(YAHOO.util.DDProxy) {
             this.setYConstraint(10, 10);            
         },
         _resizeProxy: function() {
-            this.constructor.superclass._resizeProxy.apply(this, arguments);
+            YAHOO.widget.ColumnDD.superclass._resizeProxy.apply(this, arguments);
             var dragEl = this.getDragEl(),
                 el = this.getEl();
 
