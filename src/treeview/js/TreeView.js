b9c1e6864878001dd2b81e0aed3d24a84b3bd9ab
george puckett <gpuckett@yahoo-inc.com>
2009-07-07 18:24:58 -0700

    fixed bug 2724768

--- src/treeview/js/TreeView.js
+++ src/treeview/js/TreeView.js
@@ -158,7 +158,7 @@ TV.prototype = {
     /**
      * Sets up the animation for collapsing children
      * @method setCollapseAnim
-     * @param {string} the type of animation (acceptable values defined in 
+     * @param {string} type of animation (acceptable values defined in 
      * YAHOO.widget.TVAnim)
      */
     setCollapseAnim: function(type) {
@@ -271,7 +271,7 @@ TV.prototype = {
          * @event animStart
          * @type CustomEvent
          * @param {YAHOO.widget.Node} node the node that is expanding/collapsing
-         * @parm {String} type the type of animation ("expand" or "collapse")
+         * @param {String} type the type of animation ("expand" or "collapse")
          */
         this.createEvent("animStart", this);
 
@@ -384,7 +384,7 @@ TV.prototype = {
      * Depending on <a href="YAHOO.widget.Node.html#property_propagateHighlight">nodeRef.propagateHighlight</a>, other nodes might have changed
      * @event highlightEvent
      * @type CustomEvent
-        * @param node{YAHOO.widget.Node} the node that started the change in highlighting state
+     * @param node {YAHOO.widget.Node} the node that started the change in highlighting state
     */
         this.createEvent("highlightEvent",this);
      
@@ -503,7 +503,7 @@ TV.prototype = {
      * If the &lt;LI&gt; element contains an attribute called <code>yuiConfig</code>, its contents should be a JSON-encoded object
      * as the one used in method <a href="#method_buildTreeFromObject">buildTreeFromObject</a>.
      * @method buildTreeFromMarkup
-     * @param  id{string|HTMLElement} The id of the element that contains the markup or a reference to it.
+     * @param  id {string|HTMLElement} The id of the element that contains the markup or a reference to it.
      */
     buildTreeFromMarkup: function (id) {
         this.logger.log('Building tree from existing markup');
@@ -1005,11 +1005,11 @@ TV.prototype = {
     },
 
     /**
-     * Returns the treeview node reference for an anscestor element
+     * Returns the treeview node reference for an ancestor element
      * of the node, or null if it is not contained within any node
      * in this tree.
      * @method getNodeByElement
-     * @param {HTMLElement} the element to test
+     * @param el {HTMLElement} the element to test
      * @return {YAHOO.widget.Node} a node reference or null
      */
     getNodeByElement: function(el) {
@@ -1041,7 +1041,7 @@ TV.prototype = {
      * Removes the node and its children, and optionally refreshes the 
      * branch of the tree that was affected.
      * @method removeNode
-     * @param {Node} The node to remove
+     * @param {Node} node to remove
      * @param {boolean} autoRefresh automatically refreshes branch if true
      * @return {boolean} False is there was a problem, true otherwise.
      */
@@ -1137,7 +1137,7 @@ TV.prototype = {
      * to make it possible to insert the branch into another part of the 
      * tree, or another tree.
      * @method popNode
-     * @param {Node} the node to remove
+     * @param {Node} node to remove
      */
     popNode: function(node) { 
         var p = node.parent;
