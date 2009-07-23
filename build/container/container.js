458801d0afd3de804604e9f3d7ec24d2cb1d81b6
Satyen Desai <sdesai@yahoo-inc.com>
2009-07-23 13:55:06 -0700

    Users can now prevent Dialog form submission by returning false from a beforeSubmit event subscriber

--- build/container/container.js
+++ build/container/container.js
@@ -8152,7 +8152,7 @@
         validate: function () {
             return true;
         },
-        
+
         /**
         * Executes a submit of the Dialog if validation 
         * is successful. By default the Dialog is hidden
@@ -8164,15 +8164,18 @@
         */
         submit: function () {
             if (this.validate()) {
-                this.beforeSubmitEvent.fire();
-                this.doSubmit();
-                this.submitEvent.fire();
-
-                if (this.cfg.getProperty("hideaftersubmit")) {
-                    this.hide();
+                if (this.beforeSubmitEvent.fire()) {
+                    this.doSubmit();
+                    this.submitEvent.fire();
+    
+                    if (this.cfg.getProperty("hideaftersubmit")) {
+                        this.hide();
+                    }
+    
+                    return true;
+                } else {
+                    return false;
                 }
-
-                return true;
             } else {
                 return false;
             }
