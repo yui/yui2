ae1a02ea03be1f16d38fbe04f199447472d4b6af
Todd Kloots <kloots@yahoo-inc.com>
2009-07-28 12:48:56 -0700

    Added a click event handler for a Button's <label> element that will check/uncheck buttons of type "radio" and "checkbox" [bug 2527640]

--- build/button/button-debug.js
+++ build/button/button-debug.js
@@ -2589,6 +2589,19 @@
 			
 		},        
         
+
+		_onLabelClick: function (event) {
+
+			this.focus();
+
+			var sType = this.get("type");
+
+			if (sType == "radio" || sType == "checkbox") {
+				this.set("checked", (!this.get("checked")));						
+			}
+			
+		},
+
         
         // Public methods
         
@@ -3033,7 +3046,11 @@
 
             if (oLabel) {
             
-				this.on("appendTo", setLabel);     
+				this.on("appendTo", setLabel); 
+				
+				Event.on(oLabel, "click", this._onLabelClick, null, this);
+
+				this._label = oLabel;
             
             }
             
@@ -3639,8 +3656,9 @@
             YAHOO.log("Destroying ...", "info", this.toString());
         
             var oElement = this.get("element"),
-                oParentNode = oElement.parentNode,
                 oMenu = this._menu,
+				oLabel = this._label,
+                oParentNode,
                 aButtons;
         
             if (oMenu) {
@@ -3664,6 +3682,16 @@
             Event.removeListener(document, "mouseup", this._onDocumentMouseUp);
             Event.removeListener(document, "keyup", this._onDocumentKeyUp);
             Event.removeListener(document, "mousedown", this._onDocumentMouseDown);
+
+
+			if (oLabel) {
+
+            	Event.removeListener(oLabel, "click", this._onLabelClick);
+				
+				oParentNode = oLabel.parentNode;
+				oParentNode.removeChild(oLabel);
+				
+			}
         
         
             var oForm = this.getForm();
@@ -3678,6 +3706,8 @@
             YAHOO.log("Removing CustomEvent listeners.", "info", this.toString());
 
             this.unsubscribeAll();
+
+			oParentNode = oElement.parentNode;
 
             if (oParentNode) {
 
