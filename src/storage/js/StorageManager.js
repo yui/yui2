6361f804c6028995c749383be4405fee5441e978
Alaric Cole <ALARIC@yahoo.com>
2009-08-05 13:46:15 -0700

    attempting to merge from github: removing copyright; showing the SWF displaySettings form; fixing yui doc build; removing StorageEngineCookie & StorageTest

--- src/storage/js/StorageManager.js
+++ src/storage/js/StorageManager.js
@@ -1,15 +1,3 @@
-/*
- * Copyright (c) 2009, Matt Snider, LLC. All rights reserved.
- * Version: 0.2.00
- */
-
-// todo: move the session ID method to this
-
-/**
- * The Storage module manages client-side data storage.
- * @module Storage
- */
-
 /**
  * The StorageManager class is a singleton that registers DataStorage objects and returns instances of those objects.
  * @class StorageManager
